ab91a1dc2aa5a465a5979ecbb83df49f9adbba4c
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-20 00:33:26 +0530

    passed two unit tests

--- hashthat/hashing/views.py
+++ hashthat/hashing/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from .forms import HashForm
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'hashing/home.html')
+    form = HashForm()
+    return render(request, 'hashing/home.html', {'form':form})
+    
