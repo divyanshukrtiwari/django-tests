48b79c4220b71065f9c86280a99fa7d0f600a429
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-20 00:09:30 +0530

    added first unit test

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -1,15 +1,27 @@
 from django.test import TestCase
 from selenium import webdriver
 
-class FunctionalTestCase(TestCase):
+# class FunctionalTestCase(TestCase):
     
-    def setUp(self):
-        self.browser = webdriver.Chrome()
+#     def setUp(self):
+#         self.browser = webdriver.Chrome()
 
-    def homePage_exist(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('install', self.browser.page_source)
+#     def test_homePage(self):
+#         self.browser.get('http://localhost:8000')
+#         self.assertIn('Enter text here', self.browser.page_source)
 
-    def tearDown(self):
-        self.browser.quit()
-    
+#     def test_hash_of_hello(self):
+#         self.browser.get('http://localhost:8000')
+#         text = self.browser.find_element_by_id('id_text')
+#         text.send_keys('hello')
+#         self.browser.find_element_by_name('submit').click()
+#         self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+
+#     def tearDown(self):
+#         self.browser.quit()
+
+class  UnitTestCase(TestCase):
+
+    def test_homePage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'hashing/home.html')    
