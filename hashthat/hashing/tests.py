b1b2c9df3b0b5b0af6c437332670b6fe2765fa74
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-20 16:36:34 +0530

    completed unit tests

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -5,24 +5,24 @@ import hashlib
 
 from .models import Hash
 
-# class FunctionalTestCase(TestCase):
+class FunctionalTestCase(TestCase):
     
-#     def setUp(self):
-#         self.browser = webdriver.Chrome()
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-#     def test_homePage(self):
-#         self.browser.get('http://localhost:8000')
-#         self.assertIn('Enter text here', self.browser.page_source)
+    def test_homePage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Enter text here', self.browser.page_source)
 
-#     def test_hash_of_hello(self):
-#         self.browser.get('http://localhost:8000')
-#         text = self.browser.find_element_by_id('id_text')
-#         text.send_keys('hello')
-#         self.browser.find_element_by_name('submit').click()
-#         self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
 
-#     def tearDown(self):
-#         self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
 class  UnitTestCase(TestCase):
 
